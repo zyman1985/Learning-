a08c346c7fc6b2265e1fde81a0c1c784042467ca
zyman1985 <37315977+zyman1985@users.noreply.github.com>
2018-03-15 13:27:45 +0800

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,21 @@
+name = 'Zed A.Shaw'
+age = 35 #not a lie
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s."  % name
+print "He's %d inches tall."  % height
+print "He's %.2f cm tall." %(height * 2.54)
+print "He's %d pounds heavy." % weight
+print "He's %.2f kg heavy." %(weight * 0.45)
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "He's got %r eyes and %r hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." %(
+	age, height, weight, age + height + weight)
